83d81b99e420c8c281410a776165c2087189816a
h1sk1 <44468142+h1sk1@users.noreply.github.com>
2024-11-20 07:08:05 +0000

    fix: merge default.nix

--- default.nix
+++ default.nix
@@ -12,7 +12,6 @@ let
 
 in pkgs.stdenv.mkDerivation {
   name = "xpmminer-nixos-builder-with-cmake";
-<<<<<<< HEAD
   src = ./src;
   nativeBuildInputs = [ cmake ];
   buildInputs = [ ncurses cmake curl jansson openssl gmp gcc gnumake ];
@@ -34,21 +33,3 @@ in pkgs.stdenv.mkDerivation {
     echo "xpmminer nixos builder with cmake ready!"
   '';
 }
-
-=======
-  src = ./.;
-  buildInputs = [ ncurses cmake curl jansson openssl gmp gcc gnumake ];
-  buildPhase = ''
-    mkdir -p build
-    cd build
-    cmake -DCMAKE_BUILD_TYPE=Release -DBUILDOPENCLMINER=OFF -DBUILDCUDAMINER=OFF ../src
-    make
-  '';
-  installPhase = ''
-    # Define the install phase if required
-  '';
-  shellHook = ''
-    echo "xpmminer nixos builder with cmake ready!"
-  '';
-}
->>>>>>> 2931ed3ed0c24e1b93d700be32a9b3ad09f54879
