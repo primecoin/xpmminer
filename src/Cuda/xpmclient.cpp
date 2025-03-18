0bc4bea3119f1b764feb7b51bc710a924386b860
CuHzc <1220748574>
2025-03-18 11:22:13 +0800

    nchaintype

--- src/Cuda/xpmclient.cpp
+++ src/Cuda/xpmclient.cpp
@@ -676,7 +676,7 @@ void PrimeMiner::Mining(GetBlockTemplateContext* gbp, SubmitContext* submit) {
         nOrigin = hash.shash;
         nOrigin *= multi;
         
-        testParams.nCandidateType = candi.type+1;
+        testParams.nCandidateType = candi.type+1;// nCandidateType must follow chain type convention of node
         bool isblock = ProbablePrimeChainTestFastCuda(nOrigin, testParams, mDepth);
         unsigned chainlength = TargetGetLength(testParams.nChainLength);
 
