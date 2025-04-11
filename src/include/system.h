205f2d58466be2e08c188ca4e7b6209ac3b4a526
Wwg2413 <2365277088@qq.com>
2025-04-11 09:44:11 +0800

    Revert "Improve adds statistical functions to be called in common (#22)"
    This reverts commit aa408676a8db0cc18e1aaffdfd2228f39ca95eef.

--- src/include/system.h
+++ src/include/system.h
@@ -1,33 +1,8 @@
-#ifndef SYSTEM_H
-#define SYSTEM_H
-
 #include <stdint.h>
 #include <chrono>
-#include <stdio.h>
 #if defined(__GXX_EXPERIMENTAL_CXX0X__) && (__cplusplus < 201103L)
 #define steady_clock monotonic_clock
 #endif  
-
-#define MaxChainLength 20
-
-struct PrimeSource;
-struct GetBlockTemplateContext;
-struct SubmitContext;
-struct OpenCLPlatrormContext;
-struct OpenCLDeviceContext;
-
-struct MineContext {
-    PrimeSource* primeSource;
-    GetBlockTemplateContext* gbp;
-    SubmitContext* submit;
-    unsigned threadIdx;
-    OpenCLPlatrormContext *platform;
-    OpenCLDeviceContext *device;
-    uint64_t totalRoundsNum;
-    uint64_t foundChains[MaxChainLength];
-    double speed;
-    void* log;
-};
 
 enum PathTy {
   PtExecutable = 0,
@@ -43,7 +18,3 @@ uint64_t usDiff(timeMark first, timeMark second);
 const char *installPrefix();
 const char *buildPath(PathTy type, const char *fileName);
 void xsleep(unsigned seconds);
-
-void printMiningStats(timeMark workBeginPoint, MineContext* mineCtx, int threadsNum, double sieveSizeInGb, unsigned blockHeight, double difficulty);
-
-#endif // SYSTEM_H
